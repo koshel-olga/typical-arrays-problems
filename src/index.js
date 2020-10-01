62a5fd8a531416117648fa7c6332297563d358ef
koshel-olga <koshel.olga@mail.ru>
2020-10-02 00:58:55 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
+  if ((Array.isArray(array)) && (array.length > 0) ) {
+    minimum = 0;
+    for(i=0; i<array.length; i++) {
+      if (array[i] < minimum) {
+        minimum = array[i];
+      } 
+    }
+    return minimum;
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if ((Array.isArray(array)) && (array.length > 0) ) {
+    maximum = 0;
+    for(i=0; i<array.length; i++) {
+      if (array[i] > maximum) {
+        maximum = array[i];
+      } 
+    }    
+    return maximum;
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if ((Array.isArray(array)) && (array.length > 0) ) {
+    sum = 0.;
+    for(i=0; i<array.length; i++) {
+      sum += array[i];
+    }
+    return sum/array.length;
+  }
   return 0;
 }
